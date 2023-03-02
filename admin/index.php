884ca05f8bbfb786e918b989e6f82e383c6cf316
Raphael Pitol <raphaelpitoljuliani@gmail.com>
2023-03-01 22:13:14 -0300

    Meu quinto Commit com Css coisa mais linda!!!

--- admin/index.php
+++ admin/index.php
@@ -5,12 +5,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Raphael Pitol</title>
+
+    
 </head>
 <body>
-   <h1>Meu Primeiro Commit!!!!!</h1>
+   <h1 style="background-color:DodgerBlue;">Meu Primeiro Commit!!!!!</h1>
    <h1>Meu terceiro commit</h1>
    <h1>Quarto Commit</h1>
-   <h1>Git add . para subir na are de transferencia do git</h1>
+   <h1 style="background-color:Red;">Git add . para subir na are de transferencia do git</h1>
     
     
 </body>
